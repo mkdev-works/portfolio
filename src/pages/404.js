3cc3a1f8882addd153ad9592d7f241d55f3385a6
kusumegi <kusumegi@gmail.com>
2021-03-01 21:50:45 +0900

    index,aboutページだけ作成し、初期コミット

---
+++ src/pages/404.js
@@ -0,0 +1,7 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const NotFound = () => {
+  return <div>アクセスしたページは存在しません。</div>
+}
+export default NotFound
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/layout.js"
+
+const Home = () => {
+  return (
+    <div>
+      <Layout>
+        <h1>About</h1>
+      </Layout>
+    </div>
+  )
+}
+export default Home
